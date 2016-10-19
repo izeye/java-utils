b761a75d800584e925edee2d5a693de27ffa3d84
Johnny Lim <izeye@naver.com>
2016-10-19 14:17:08 +0900

    Add a String utility for field replacement

---
+++ src/main/java/com/izeye/util/StringUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izeye.util;
+
+import lombok.Data;
+
+/**
+ * Utilities for {@link String}.
+ *
+ * @author Johnny Lim
+ */
+public abstract class StringUtils {
+
+	private StringUtils() {
+	}
+
+	public static String replaceField(
+			String target, FieldDescription targetFieldDescription, String replacement) {
+		int beforeDelimiterIndex = 0;
+		int afterDelimiterIndex = 0;
+		for (FieldDescription fd = targetFieldDescription; fd != null; fd = fd.getSubFieldDescription()) {
+			char delimiter = fd.getDelimiter();
+			for (int i = 0; i < fd.getIndex(); i++) {
+				beforeDelimiterIndex = target.indexOf(delimiter, beforeDelimiterIndex + 1);
+			}
+			afterDelimiterIndex = target.indexOf(delimiter, beforeDelimiterIndex + 1);
+		}
+		return target.substring(0, beforeDelimiterIndex + 1) + replacement +
+				target.substring(afterDelimiterIndex);
+	}
+
+	/**
+	 * Field description.
+	 */
+	@Data
+	public static class FieldDescription {
+
+		private char delimiter;
+		private int index;
+		private FieldDescription subFieldDescription;
+
+	}
+
+}
---
+++ src/test/java/com/izeye/util/StringUtilsTests.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izeye.util;
+
+import org.junit.Test;
+
+import com.izeye.util.StringUtils.FieldDescription;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link StringUtils}.
+ *
+ * @author Johnny Lim
+ */
+public class StringUtilsTests {
+
+	@Test
+	public void testReplaceField() {
+		String expected = "1,2,3,4\t5,6,100,8\t9,10,11,12";
+		String target = "1,2,3,4\t5,6,7,8\t9,10,11,12";
+		String replacement = "100";
+
+		FieldDescription targetFieldDescription = new FieldDescription();
+		targetFieldDescription.setDelimiter('\t');
+		targetFieldDescription.setIndex(1);
+		FieldDescription subFieldDescription = new FieldDescription();
+		subFieldDescription.setDelimiter(',');
+		subFieldDescription.setIndex(2);
+		targetFieldDescription.setSubFieldDescription(subFieldDescription);
+
+		String replaced = StringUtils.replaceField(target, targetFieldDescription, replacement);
+		assertThat(replaced).isEqualTo(expected);
+	}
+
+}
