fd521010ad554c982b0bcf0b485dcf3c9856e094
Johnny Lim <izeye@naver.com>
2016-12-03 10:46:59 +0900

    Add service metrics collector

---
+++ src/main/java/com/izeye/util/metrics/DefaultServiceMetricsCollector.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izeye.util.metrics;
+
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ * Default implementation for {@link ServiceMetricsCollector}.
+ *
+ * @author Johnny Lim
+ */
+public class DefaultServiceMetricsCollector implements ServiceMetricsCollector {
+
+	private final int timeoutInMillis;
+
+	private LongAdder requestCount = new LongAdder();
+	private LongAdder timeoutCount = new LongAdder();
+	private LongAdder accumulatedProcessTimeInMillis = new LongAdder();
+
+	public DefaultServiceMetricsCollector(int timeoutInMillis) {
+		this.timeoutInMillis = timeoutInMillis;
+	}
+
+	@Override
+	public void collect(int processTimeInMillis) {
+		this.requestCount.increment();
+		if (processTimeInMillis > timeoutInMillis) {
+			this.timeoutCount.increment();
+		}
+		this.accumulatedProcessTimeInMillis.add(processTimeInMillis);
+	}
+
+	@Override
+	public ServiceMetrics getThenReset() {
+		long requestCount = this.requestCount.sumThenReset();
+		long timeoutCount = this.timeoutCount.sumThenReset();
+		long accumulatedProcessTimeInMillis = this.accumulatedProcessTimeInMillis.sumThenReset();
+		long averageProcessTimeInMillis =
+				requestCount == 0L ? 0L : accumulatedProcessTimeInMillis / requestCount;
+		return new ServiceMetrics(requestCount, timeoutCount, averageProcessTimeInMillis);
+	}
+
+}
---
+++ src/test/java/com/izeye/util/metrics/ServiceMetricsCollectorTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izeye.util.metrics;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link ServiceMetricsCollector}.
+ *
+ * @author Johnny Lim
+ */
+public class ServiceMetricsCollectorTests {
+
+	@Test
+	public void test() {
+		int timeoutInMillis = 100;
+		ServiceMetricsCollector serviceMetricsCollector =
+				new DefaultServiceMetricsCollector(timeoutInMillis);
+
+		serviceMetricsCollector.collect(50);
+		serviceMetricsCollector.collect(99);
+		serviceMetricsCollector.collect(100);
+		serviceMetricsCollector.collect(101);
+		serviceMetricsCollector.collect(1000);
+		ServiceMetrics serviceMetrics = serviceMetricsCollector.getThenReset();
+		System.out.println(serviceMetrics);
+		assertThat(serviceMetrics.getRequestCount()).isEqualTo(5);
+		assertThat(serviceMetrics.getTimeoutCount()).isEqualTo(2);
+		assertThat(serviceMetrics.getAverageProcessTimeInMillis())
+				.isEqualTo((50 + 99 + 100 + 101 + 1000) / 5);
+
+		serviceMetrics = serviceMetricsCollector.getThenReset();
+		System.out.println(serviceMetrics);
+		assertThat(serviceMetrics.getRequestCount()).isEqualTo(0);
+		assertThat(serviceMetrics.getTimeoutCount()).isEqualTo(0);
+		assertThat(serviceMetrics.getAverageProcessTimeInMillis()).isEqualTo(0);
+
+		serviceMetricsCollector.collect(50);
+		serviceMetricsCollector.collect(99);
+		serviceMetricsCollector.collect(100);
+		serviceMetricsCollector.collect(101);
+		serviceMetricsCollector.collect(1000);
+		serviceMetrics = serviceMetricsCollector.getThenReset();
+		System.out.println(serviceMetrics);
+		assertThat(serviceMetrics.getRequestCount()).isEqualTo(5);
+		assertThat(serviceMetrics.getTimeoutCount()).isEqualTo(2);
+		assertThat(serviceMetrics.getAverageProcessTimeInMillis())
+				.isEqualTo((50 + 99 + 100 + 101 + 1000) / 5);
+	}
+
+}
