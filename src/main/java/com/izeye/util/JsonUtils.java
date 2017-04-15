28306dd3d8060fc1abb6e1fcf2d3c1229baffe17
Johnny Lim <izeye@naver.com>
2017-04-15 10:51:59 +0900

    Add utilities for JSON

---
+++ src/main/java/com/izeye/util/JsonUtils.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izeye.util;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Utilities for JSON.
+ *
+ * @author Johnny Lim
+ */
+public final class JsonUtils {
+
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+
+	private JsonUtils() {
+	}
+
+	public static String toJson(Object object) {
+		StringWriter writer = new StringWriter();
+		try {
+			MAPPER.writeValue(writer, object);
+			return writer.toString();
+		}
+		catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+}
---
+++ src/test/java/com/izeye/util/JsonUtilsTests.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izeye.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link JsonUtils}.
+ *
+ * @author Johnny Lim
+ */
+public class JsonUtilsTests {
+
+	@Test
+	public void testToJson() {
+		Person person = new Person("Johnny", 20);
+		String json = JsonUtils.toJson(person);
+		assertThat(json).isEqualTo("{\"name\":\"Johnny\",\"age\":20}");
+	}
+
+	@Data
+	@AllArgsConstructor
+	private static class Person {
+
+		private String name;
+		private int age;
+
+	}
+
+}
